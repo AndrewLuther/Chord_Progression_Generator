c744e161be7163fc6394197c4572a7714b30bbe5
Andrew Luther <90206207+AndrewLuther@users.noreply.github.com>
2022-04-04 12:16:54 -0230

    chord print

--- Chord.java
+++ Chord.java
@@ -18,6 +18,11 @@ public class Chord {
         }
     }
 
+    @Override
+    public String toString() {
+        return root.getName() + quality.toString();
+    }
+
 
     
 }
--- ChordQuality.java
+++ ChordQuality.java
@@ -5,14 +5,21 @@ public enum ChordQuality {
     MAJOR("Major", new ArrayList<Integer>(List.of(0,4,7))),
     MINOR("Minor", new ArrayList<Integer>(List.of(0,3,7)));
 
+    private final String name;
     private final ArrayList<Integer> intervals;
     
     ChordQuality(String name, ArrayList<Integer> intervals){
         this.intervals = intervals;
+        this.name = name;
     }
 
     public ArrayList<Integer> getIntervals(){
         return intervals;
+    }
+
+    @Override
+    public String toString(){
+        return name;
     }
 
 
