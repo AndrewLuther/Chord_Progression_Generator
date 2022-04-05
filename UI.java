d48edf04e1e44a6780234154db6946c4f158ccb4
Andrew Luther <90206207+AndrewLuther@users.noreply.github.com>
2022-04-05 20:54:12 -0230

    Update UI.java

--- UI.java
+++ UI.java
@@ -14,13 +14,29 @@ public class UI {
             String rootInput = input.nextLine();
             try {
                 Note root = readNote(rootInput);
-                System.out.println(root.getAltName());
+                //System.out.println(root.getAltName());
             }
             catch(Exception e) {
-                System.out.println("Invalid Input!");
                 inputStr = rootInput;
+                if(rootInput.equals("exit")) return;
+                System.out.println("Invalid Input!");
                 continue;
             }
+
+            System.out.println("Enter a Scale:");
+            String scaleInput = input.nextLine();
+            try {
+                ScaleType scale = readScale(scaleInput);
+                //System.out.println(root.getAltName());
+            }
+            catch(Exception e) {
+                inputStr = scaleInput;
+                if(scaleInput.equals("exit")) return;
+                System.out.println("Invalid Input!");
+                continue;
+            }
+
+
         }
     
     }
@@ -29,11 +45,23 @@ public class UI {
         Note[] notes = Note.values();
         for(int i = 0; i<notes.length; i++){
             Note note = notes[i];
-            System.out.println("Checking " + note.getName());
+            //System.out.println("Checking " + note.getName());
             if(note.getName().equals(noteInput) || note.getAltName().equals(noteInput))  return note;
             else continue;
         }
         throw new Exception("Note not found");
+
+    }
+
+    private ScaleType readScale(String scaleInput) throws Exception{
+        ScaleType[] scales = ScaleType.values();
+        for(int i = 0; i<scales.length; i++){
+            ScaleType scale = scales[i];
+            //System.out.println("Checking " + note.getName());
+            if(scale.getName().equals(scaleInput))  return scale;
+            else continue;
+        }
+        throw new Exception("Scale not found");
 
     }
     
